b615ffee998ffc49f90cd61744479af309da93e2
sh4869 <nobuk4869@gmail.com>
2020-09-02 04:26:10 +0900

    [server] implements /commit/:name/:repo/commi_status/:count

---
+++ server/evaluation/analysis_repo.go
@@ -0,0 +1,79 @@
+package evaluation
+
+import (
+	"context"
+
+	"github.com/google/go-github/v32/github"
+)
+
+func getCommitCount(client *github.Client, name string, repo string) (int, error) {
+	_, resp, err := client.Repositories.ListCommits(context.Background(), name, repo, &github.CommitsListOptions{ListOptions: github.ListOptions{PerPage: 1, Page: 1}})
+	if err != nil {
+		return 0, err
+	}
+	return resp.LastPage, err
+}
+
+type CommitStatus struct {
+	CommitPerHour   float64 `json:"commit_per_hour"`
+	AdditionPerHour int     `json:"addition_per_hour"`
+	DeletionPerHour int     `json:"deletion_per_hour"`
+}
+
+func GetCommitStatus(client *github.Client, name string, repo string, count int) (*CommitStatus, error) {
+	const length = 10
+	list, err := getNearCommitList(client, name, repo, count, length)
+	if err != nil {
+		return nil, err
+	}
+	durHour := 0.0
+	for v := range list {
+		if v != 0 {
+			before := list[v-1].Commit.Committer.Date
+			after := list[v].Commit.Committer.Date
+			// TODO: コミット期間があまりにも離れている場合は修正する
+			durHour += before.Sub(*after).Hours()
+		}
+	}
+	durAve := durHour / float64(len(list)-1)
+	compare, _, err := client.Repositories.CompareCommits(context.Background(), name, repo, *(list[len(list)-1].SHA), *(list[0].SHA))
+	if err != nil {
+		return nil, err
+	}
+	add := 0
+	del := 0
+	for i := range compare.Files {
+		f := compare.Files[i]
+		add += *f.Additions
+		del += *f.Deletions
+	}
+	return &CommitStatus{durAve, add / int(durHour), del / int(durHour)}, nil
+}
+
+func getNearCommitList(client *github.Client, name string, repo string, count int, length int) ([]*github.RepositoryCommit, error) {
+	// 直近10ミットを取るだけなのに100取るのは結構大変だが、リクエスト回数は少ないほうがいいのでこうした
+	// countの公約数で計算すればいいが、countが100を超える素数の場合は結局いかの方法を取らないといけないので
+	const perPage = 100
+	page := count / perPage
+	if page == 0 {
+		page = 1
+	}
+	list := make([]*github.RepositoryCommit, length)
+	l, _, err := client.Repositories.ListCommits(context.Background(), name, repo, &github.CommitsListOptions{ListOptions: github.ListOptions{PerPage: perPage, Page: page}})
+	if err != nil {
+		return list, err
+	}
+	rest := count%perPage - 1
+	if rest < 90 {
+		list = l[rest : rest+10]
+	}
+	// 90以上の場合は次のページを取得してリストに結合する
+	if rest > 90 {
+		nl, _, err := client.Repositories.ListCommits(context.Background(), name, repo, &github.CommitsListOptions{ListOptions: github.ListOptions{PerPage: perPage, Page: page + 1}})
+		if err != nil {
+			return list, err
+		}
+		list = append(l[rest:100], nl[0:rest-90]...)
+	}
+	return list, nil
+}
